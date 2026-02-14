e19959d2f7a6173957b3d006b412acaa7ca1caff
lesgoluqman <luqmanhzukefli@gmail.com>
2026-02-14 13:53:45 +0800

    Quartz sync: Feb 14, 2026, 1:53 PM

---
+++ content/first post.md
@@ -0,0 +1,8 @@
+---
+title: My First Post
+date: 2026-02-14
+---
+
+# My First Post
+
+Umar al Khattab
--- quartz.config.ts
+++ quartz.config.ts
@@ -29,12 +29,12 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
-          lightgray: "#e5e5e5",
+          light: "#ffffff",
+          lightgray: "#f5f5f5",
           gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
-          secondary: "#284b63",
+          darkgray: "#1a1a1a",
+          dark: "#000000",
+          secondary: "#1a1a1a",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#fff23688",
